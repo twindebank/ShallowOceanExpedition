5ecb5e5778d70de7239335989f70de91f2122dfe
windet01 <theo.windebank@bbc.co.uk>
2018-05-23 22:50:30 +0100

    tidy settings

--- ShallowOceanExpedition/utils/pretty_plot.py
+++ ShallowOceanExpedition/utils/pretty_plot.py
@@ -1,21 +1,26 @@
-import os
-
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_template import FigureCanvas
 from matplotlib.figure import Figure
 
 
 class PrettyPlot:
+    # general settings
     subplot_height = 6
     subplot_width = 10
     padding = 0
     hide_ax_borders = True
     hide_ax_ticks = True
+    bar_width = 0.85
+
+    # colors
+    title_color = '#555555'
+    axis_label_color = '#6E6D6C'
+    grid_color = 'grey'
+    bar_color = '#5169A7'
+    axis_number_color = '#6E6D6C'
 
     def __init__(self, n_rows=1, n_cols=1):
         self.fig = Figure(
             figsize=(self.subplot_width * n_cols + self.padding, self.subplot_height * n_rows + self.padding),
-            # defaults to rc figure.figsize
             edgecolor='white'
         )
         self.canvas = FigureCanvas(self.fig)
@@ -23,9 +28,9 @@ class PrettyPlot:
     def add_bar_chart(self, bar_vals, bar_labels, x_axis_label='', y_axis_label='', position=111, title=''):
         ax = self.fig.add_subplot(position)
 
-        ax.set_title(title, fontsize=10, color='#555555')
-        ax.set_xlabel(x_axis_label, fontsize=12, color='#6E6D6C')
-        ax.set_ylabel(y_axis_label, fontsize=12, color='#6E6D6C')
+        ax.set_title(title, fontsize=10, color=self.title_color)
+        ax.set_xlabel(x_axis_label, fontsize=12, color=self.axis_label_color)
+        ax.set_ylabel(y_axis_label, fontsize=12, color=self.axis_label_color)
 
         if self.hide_ax_borders:
             ax.spines["top"].set_visible(False)
@@ -41,26 +46,26 @@ class PrettyPlot:
                 left='off',  # ticks along the left edge are off
                 right='off')  # ticks along the right edge are off
 
-        ax.yaxis.grid(True, color='grey', linestyle='-', alpha=0.4, zorder=0)
-        ax.tick_params(axis='both', colors='#6E6D6C')
+        ax.yaxis.grid(True, color=self.grid_color, linestyle='-', alpha=0.4, zorder=0)
+        ax.tick_params(axis='both', colors=self.axis_number_color)
         ax.set_ylim(0, 1.3 * max(bar_vals))
         rects = ax.bar(
             bar_labels,
             bar_vals,
-            width=0.85,
-            color='#5169A7',
+            width=self.bar_width,
+            color=self.bar_color,
             alpha=1,
             edgecolor='white',
             zorder=3,
             align='center'
         )
-        autolabel(ax, rects)
+        autolabel(ax, rects, self.bar_color)
 
     def save_fig(self, filepath):
         self.fig.savefig(filepath, bbox_inches='tight')
 
 
-def autolabel(ax, rects):
+def autolabel(ax, rects, color):
     """
     Attach a text label above each bar displaying its height
     """
@@ -68,4 +73,4 @@ def autolabel(ax, rects):
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
                 '%d' % int(height),
-                ha='center', va='bottom', color='#5169A7', weight='bold')
+                ha='center', va='bottom', color=color, weight='bold')
